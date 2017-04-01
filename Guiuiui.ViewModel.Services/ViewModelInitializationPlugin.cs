0b3611f31c2a5b32233fe008ff0448c090e88942
Jonas Aklin <jonas.aklin@gmail.com>
2017-04-02 00:43:04 +0200

    IConversion deleted, ITextConverter added...

--- Guiuiui.ViewModel.Services/ViewModelInitializationPlugin.cs
+++ Guiuiui.ViewModel.Services/ViewModelInitializationPlugin.cs
@@ -8,9 +8,7 @@ namespace Guiuiui.ViewModel.Services
 {
     using Guiuiui.Base.InversionOfControl;
     using Guiuiui.Base.StartUp;
-    using Guiuiui.ViewModel.Services.Conversion;
     using Parser;
-    using ViewModel.Conversion;
     using ViewModel.Parser;
 
     [ProjectInitializationPlugin]
@@ -23,15 +21,6 @@ namespace Guiuiui.ViewModel.Services
             // Parser
             Ioc.Container.RegisterSingleton<IParser<int>, IntParser>();
             Ioc.Container.RegisterSingleton<IParser<string>, StringDummyParser>();
-
-            // Conversions
-            Ioc.Container.RegisterSingleton<IConversion<string, int>, StringToIntConversion>();
-
-            Ioc.Container.RegisterSingleton<IConversion<bool, bool>, DummyConversion<bool>>();
-            Ioc.Container.RegisterSingleton<IConversion<int, int>, DummyConversion<int>>();
-            Ioc.Container.RegisterSingleton<IConversion<double, double>, DummyConversion<double>>();
-            Ioc.Container.RegisterSingleton<IConversion<decimal, decimal>, DummyConversion<decimal>>();
-            Ioc.Container.RegisterSingleton<IConversion<string, string>, DummyConversion<string>>();
         }
     }
 }
---
+++ Guiuiui.ViewModel/TextConverter/ITextConverter.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlowMeInTheShoes.cs" company="Jonas Aklin">
+//     Copyright (c) Jonas Aklin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Guiuiui.ViewModel.TextConverter
+{
+    /// <summary>
+    /// Provides a method to convert a <typeparamref name="TValue"/> to a user-readable text.
+    /// </summary>
+    /// <typeparam name="TValue">
+    /// Type of the value which is converted
+    /// </typeparam>
+    public interface ITextConverter<TValue>
+    {
+        /// <summary>
+        /// Convert a <typeparamref name="TValue"/> to a user-readable text.
+        /// </summary>
+        /// <param name="value">
+        /// Can be <c>null</c>.
+        /// </param>
+        string GetText(TValue value);
+    }
+}
