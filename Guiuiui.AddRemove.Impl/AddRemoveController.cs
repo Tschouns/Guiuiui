8c33017a37e7c5b615285df549fdee859f0a79fd
Tschouns <jonas.aklin@gmail.com>
2017-05-20 14:34:16 +0200

    AddRemove projects added

---
+++ Guiuiui.AddRemove.Impl/AddRemoveController.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlowMeInTheShoes.cs" company="Jonas Aklin">
+//     Copyright (c) Jonas Aklin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Guiuiui.AddRemove.Impl
+{
+    using System;
+
+    /// <summary>
+    /// Implements <see cref="IAddRemove"/>.
+    /// </summary>
+    public class AddRemoveController : IAddRemove
+    {
+        /// <summary>
+        /// See <see cref="IAddRemove.Add"/>.
+        /// </summary>
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// See <see cref="IAddRemove.Remove"/>.
+        /// </summary>
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Guiuiui.AddRemove/IAddRemove.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BlowMeInTheShoes.cs" company="Jonas Aklin">
+//     Copyright (c) Jonas Aklin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Guiuiui.AddRemove
+{
+    /// <summary>
+    /// Allows to initiate "add" and "remove" operations.
+    /// </summary>
+    public interface IAddRemove
+    {
+        /// <summary>
+        /// Initiates an "add" operation.
+        /// </summary>
+        void Add();
+
+        /// <summary>
+        /// Initiates a "remove" operation.
+        /// </summary>
+        void Remove();
+    }
+}
